a409bf0bbc57248dcf2076b3e83f0b3964a40e2e
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-24 14:37:04 +0530

    http responses and routing using node was done

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((request,response)=>{
-    console.log("Request made")
+
+    let path = './folder1/'
+    console.log(request.url);
+
+    response.setHeader('Content-Type','text/html');
+
+    if(request.url == '/'){
+        path += 'index.html'
+    }
+    else if(request.url == '/home'){
+        response.statusCode=301;
+        response.setHeader('Location','/');
+        response.end();
+    }
+    else if(request.url == '/page1'){
+        path += 'page1.html'
+        response.statusCode= 200;
+    }
+    else if(request.url == '/about'){
+        path += 'about.html'
+        response.statusCode = 200;
+    }
+    else{
+        path += 'error.html'
+        response.statusCode = 404;
+    }
+   
+    fs.readFile(path,(err,data)=>{
+        if(err){
+            console.log(err.message);
+            response.end();
+        }
+        else{
+            // response.write(data)
+            // response.end();
+            response.end(data)
+        }
+    })
 })
 
 server.listen(3000,'localhost',()=>{
